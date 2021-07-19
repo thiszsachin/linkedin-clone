b51f61faebca210d778757388b5351a83dc27e07
Sachin Srivastava <thiszsachin@gmail.com>
2021-07-20 00:06:57 +0530

    icon added

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,10 +7,11 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import { Link } from "@material-ui/core";
+import { Avatar, Link } from "@material-ui/core";
 import PersonAddIcon from "@material-ui/icons/PersonAdd";
 import LaunchIcon from "@material-ui/icons/Launch";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import displayPic from "../../static/images/displayPic.jfif";
 
 const useStyles = makeStyles({
   root: {
@@ -27,11 +28,29 @@ export default function Profile() {
   return (
     <Card className={classes.root}>
       <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="https://media-exp1.licdn.com/dms/image/C561BAQHXuBYDcBUm2w/company-background_10000/0/1620411660155?e=1626728400&v=beta&t=_bKyCyGKabdFPjQfLPB0jOCqvnX9AgOYtf3dT4ARZ1E"
-          title="Contemplative Reptile"
-        />
+        <div style={{ position: "relative" }}>
+          <CardMedia
+            className={classes.media}
+            image="https://media-exp1.licdn.com/dms/image/C561BAQHXuBYDcBUm2w/company-background_10000/0/1620411660155?e=1626728400&v=beta&t=_bKyCyGKabdFPjQfLPB0jOCqvnX9AgOYtf3dT4ARZ1E"
+            title="Contemplative Reptile"
+          />
+
+          <Avatar
+            // size="200px"
+            style={{
+              postion: "absolute",
+              top: "-50px",
+              right: "-30px",
+              width: "100px",
+              height: "100px",
+              borderRadius: "12px",
+              boxShadow: "2px 2px grey",
+            }}
+            variant="square"
+            alt="Remy Sharp"
+            src={displayPic}
+          />
+        </div>
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
             Figma
